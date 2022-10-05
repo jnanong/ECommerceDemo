a793c4f7d0fa0734d012bc7fe6d5d4c162aa772f
jnanong <john.nanong@gmail.com>
2022-10-05 20:05:54 +0800

    End of Section 3. Override the package.json because of some dependency issues.

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,19 +1,94 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
 
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'about', path: '/about'},
+    {title: 'contact', path: '/contact'}
+]
+
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'register', path: '/register'}
+]
+
+const navStyles = {
+    color: 'inherit',
+    textDecoration: 'none',
+    '&:hover': {
+        color: 'secondary.main'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
+
 export default function Header({darkMode, handleThemeChange} : Props) {
     return (
         <>
             <AppBar position="static" sx={{ mb : 4 }}>
-                <Toolbar>
-                    <Typography variant='h6'>
-                        RE-STORE
-                    </Typography>
-                    <Switch checked={darkMode} onChange={handleThemeChange}/>
+                <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+
+                    <Box display='flex' alignItems='center'>
+                        <Typography variant='h6'
+                            exact
+                            component={NavLink} to='/'
+                            sx={navStyles}>
+                            RE-STORE
+                        </Typography>
+                        <Switch checked={darkMode} onChange={handleThemeChange} />
+                    </Box>
+
+                    <List sx={{display: 'flex', flexDirection: 'horizontal'}}>
+                        {midLinks.map(({title, path}) => (
+                                <ListItem
+                                    component={NavLink}
+                                    to={path}
+                                    key={path}
+                                    sx={{
+                                        color: 'inherit',
+                                        typography: 'h6',
+                                        '&:hover' : {
+                                            color: 'grey.500'
+                                        },
+                                        '&.active' : {
+                                            color: 'text.secondary'
+                                        }
+                                    }}
+                                >
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))}
+                    </List>
+
+                    <Box display='flex' alignItems='center' >
+                        <IconButton
+                            size='large' sx={navStyles}>
+                            <Badge badgeContent={4} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                        <List sx={{ display: 'flex', flexDirection: 'horizontal' }}>
+                            {
+                                rightLinks.map(({ title, path }) => (
+                                    <ListItem
+                                        component={NavLink}
+                                        to={path}
+                                        key={path}
+                                        sx={navStyles}
+                                    >
+                                        {title.toUpperCase()}
+                                    </ListItem>
+                                ))
+                            }
+                        </List>
+                    </Box>
                 </Toolbar>
             </AppBar>
         </>
